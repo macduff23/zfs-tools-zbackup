8d4ee5331e9ddc7c3d335ebd96c63e1f1fb85184
Simon Guest <simon.guest@tesujimath.org>
2018-12-14 11:33:05 +1300

    Updated packaging for PyPI.

--- setup.py
+++ setup.py
@@ -6,12 +6,6 @@ import os
 dir = os.path.dirname(__file__)
 path_to_main_file = os.path.join(dir, "src/zfstools/__init__.py")
 path_to_readme = os.path.join(dir, "README.md")
-for line in open(path_to_main_file):
-	if line.startswith('__version__'):
-		version = line.split()[-1].strip("'").strip('"')
-		break
-else:
-	raise ValueError, '"__version__" not found in "src/zfstools/__init__.py"'
 readme = open(path_to_readme).read(-1)
 
 classifiers = [
@@ -31,14 +25,15 @@ classifiers = [
 ]
 
 setup(
-	name='zfs-tools',
-	version=version,
+	name='zfs-tools-zbackup',
+        use_scm_version=True,
+        setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
 	description='ZFS synchronization and snapshotting tools',
 	long_description = readme,
-	author='Manuel Amador (Rudd-O)',
-	author_email='rudd-o@rudd-o.com',
+	author='Manuel Amador (Rudd-O), Simon Guest (tesujimath)',
+	author_email='rudd-o@rudd-o.com, simon.guest@tesujimath.org',
 	license="GPL",
-	url='http://github.com/Rudd-O/zfs-tools',
+	url='http://github.com/tesujimath/zfs-tools-zbackup',
 	package_dir=dict([
 					("", "src"),
 					]),
@@ -47,4 +42,5 @@ setup(
 	scripts=["bin/zreplicate", 'bin/zfs-shell', 'bin/zsnap', 'bin/zbackup', 'bin/zflock'],
 	keywords="ZFS filesystems backup synchronization snapshot",
 	zip_safe=False,
+        python_requires='>=2.7, <3',
 )
